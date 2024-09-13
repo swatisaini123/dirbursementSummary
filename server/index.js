40416e5b6aa9a5d63f089bcb474625fbde5f17e0
swatisaini123 <113428077+swatisaini123@users.noreply.github.com>
2024-09-13 16:06:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ var https = require('https');
 var chalk = require('chalk');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 3004;
 
 // Enable CORS
 app.use(cors());
